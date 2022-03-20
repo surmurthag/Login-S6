c7f67228f0f6d1dcefb0ae55e2d9c24b10052ea4
surmurthag <surmurthag@outlook.fr>
2022-03-20 16:26:10 +0100

    login error repair

--- src/Security/AppLoginAuthenticator.php
+++ src/Security/AppLoginAuthenticator.php
@@ -50,12 +50,18 @@ class AppLoginAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('app_home'));
+
     }
 
     protected function getLoginUrl(Request $request): string
     {
         return $this->urlGenerator->generate(self::LOGIN_ROUTE);
     }
+
+    public function supports(Request $request): bool
+    {
+        return $request->isMethod('POST') && self::LOGIN_ROUTE === $request->attributes->get('_route');
+    }
+
 }
